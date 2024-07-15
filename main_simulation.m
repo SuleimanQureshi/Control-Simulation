20d3f0880b5c533ca75c3cebc6a9b9c6930a020c
SuleimanQureshi <mq06812@st.habib.edu.pk>
2024-07-15 18:58:34 +0500

    Minor maneuvering error when changing penultimate point. Else, works well.

--- main_simulation.m
+++ main_simulation.m
@@ -53,14 +53,6 @@ t_stop = 15;
 T = t_start:dt:t_stop;
 
 
-
-%Reference Trajectory
-global x_ref;
-global y_ref;
-radius = 6;
-% x_ref = radius*cos(2*pi*T/t_stop-pi/2) ;
-% y_ref = radius*sin(2*pi*T/t_stop-pi/2) + 6;
-
 waypoints = [            
             5 1;
             15 5;
@@ -140,40 +132,11 @@ function [X_Dot, V_1_2] = transf_func(X_prev,u)
     ];
 end
 
-global test_int;
-test_int = zeros(2,length(T));
-global testing_desired_angle;
 
-global angle_error;
-angle_error = zeros(1,length(T));
-
-global distance_error;
-angle_error = zeros(1,length(T));
-
-global another_test;
-another_test = zeros(1,length(T));
-
-global another_test2;
-another_test2 = zeros(1,length(T));
-
-
-testing_desired_angle = zeros(2,length(T));
-
-function [x] = manual_mod(a,b)
-    x = a-fix(a/b)*b;
-end
 
 function  [v_path, phi_path, e_prev, int_val,current_waypoint] = PID(X_prev,t,int_val,k_gains,epsilon,e_prev,waypoints,current_waypoint,prev_vel,prev_phi)
-    global x_ref
-    global y_ref
     global dt
     global Lw0
-    global testing_desired_angle;
-    global test_int;   
-    global distance_error;
-    global angle_error;
-    global another_test2;
-
 
     % NOTE: these aren't real values like m/s^2 etc, these are just
     % temporary values for PoC, these will be experimentally gathered and
@@ -187,15 +150,13 @@ function  [v_path, phi_path, e_prev, int_val,current_waypoint] = PID(X_prev,t,in
     e_x = waypoints(current_waypoint,1)-X_prev(1);
     e_y = waypoints(current_waypoint,2)-X_prev(2);
 
-    psi = (atan2(e_y,e_x)) - X_prev(3);
-    % psi = manual_mod(psi,2*pi);
+    psi = (atan2(e_y,e_x)) - wrapToPi(X_prev(3));
+
     psi = wrapToPi(psi);
-    testing_desired_angle(1,t) =  psi;
     row = sqrt(e_x^2 + e_y^2);
-    distance_error(t) = row;
 
     %changing waypoints
-    wp_dist = 0.5; % distance to change waypoints
+    wp_dist = 0.2; % distance to change waypoints
     if (row <= wp_dist)
         if (current_waypoint ~= length(waypoints))
             current_waypoint=current_waypoint + 1;
@@ -204,8 +165,7 @@ function  [v_path, phi_path, e_prev, int_val,current_waypoint] = PID(X_prev,t,in
             phi_path = 0;
             return
         end
-        % x_ref_dot = (waypoints(current_waypoint-1,1)-waypoints(current_waypoint,1))/dt;
-        % y_ref_dot = (waypoints(current_waypoint-1,2)-waypoints(current_waypoint,2))/dt;
+
         x_ref_dot = 0;
         y_ref_dot = 0;
         %recalculate everything
@@ -213,10 +173,7 @@ function  [v_path, phi_path, e_prev, int_val,current_waypoint] = PID(X_prev,t,in
         e_y = waypoints(current_waypoint,2)-X_prev(2);
     
         psi = (atan2(e_y,e_x)) - X_prev(3);
-        % psi = manual_mod(psi,2*pi);
         psi = wrapToPi(psi);
-
-        testing_desired_angle(1,t) =  psi;
 
         row = sqrt(e_x^2 + e_y^2);
     else
@@ -224,22 +181,15 @@ function  [v_path, phi_path, e_prev, int_val,current_waypoint] = PID(X_prev,t,in
         y_ref_dot = 0;
     end
     
-    angle_error(t) = psi;
-
     psi_int = int_val(1) + dt*psi;
     row_int = int_val(2) + dt*row;
     int_val = [psi_int row_int];
     
     if (abs(psi) < epsilon)
         v_path = (k_gains(3)*row+k_gains(4)*row_int)/cos(psi) + (e_x*x_ref_dot+e_y*y_ref_dot)/(row*cos(psi));
-        % v_path = k_gains(3)*row+k_gains(4)*row_int;
     else
-        disp("here")
-        another_test2(t) = 5;
-        v_path = 2;
+        v_path = 1;
         phi_path = (psi)/abs(psi)*phi_limit;
-        % x = (psi);
-        % phi_path = (wrapToPi(x))/abs(wrapToPi(x))*phi_limit;
 
         if (abs(phi_path) > phi_limit)
         phi_path = phi_limit*abs(phi_path)/phi_path;
@@ -256,41 +206,28 @@ function  [v_path, phi_path, e_prev, int_val,current_waypoint] = PID(X_prev,t,in
     
         if ((phi_path-prev_phi) > ang_vel_limit)
             phi_path = prev_phi + ang_vel_limit;
-            disp("123")
         end
     
         if ((phi_path-prev_phi) < -ang_vel_limit)
             phi_path = prev_phi - ang_vel_limit;
-                        disp("456")
 
         end
     
         if (abs(phi_path) > phi_limit)
             phi_path = phi_limit*abs(phi_path)/phi_path;
-                        disp("789")
-
         end
     
         if (abs(v_path) > v_limit)
             v_path = abs(v_path)/v_path*v_limit;
-                        disp("abc")
-
         end
         e_prev = [e_x, e_y];
         return
 
     end
     tan_dev = ((atan2(e_y,e_x)) - (atan2(e_prev(2),e_prev(1))))/dt;
-    % phi_path = k_gains(1)*psi+k_gains(2)*psi_int + tan_dev;
-    % test_int(:,t) = [(k_gains(3)*row+k_gains(4)*row_int)/cos(psi), (e_x*x_ref_dot+e_y*y_ref_dot)/(row*cos(psi)); ];
-
-
     theta_dot_PID = k_gains(1)*psi+k_gains(2)*psi_int + tan_dev;
-    % theta_dot_PID = k_gains(1)*psi+k_gains(2)*psi_int 
 
     phi_path = (atan2(theta_dot_PID*Lw0,v_path));
-    testing_desired_angle(1,t) =  phi_path ;
-
     
     if (abs(phi_path) > phi_limit)
         phi_path = phi_limit*abs(phi_path)/phi_path;
@@ -326,26 +263,19 @@ function  [v_path, phi_path, e_prev, int_val,current_waypoint] = PID(X_prev,t,in
     e_prev = [e_x, e_y];
 
 
-
-    test_int(1,t) = v_path;
-    testing_desired_angle(2,t) =  phi_path ;
     
 end
 
-theta_0_temp_test = zeros(1,length(T));
 % Initialization of PID values
-k_gains = [ 32 0.25 2.55 0.3]; % sorta tuned
-epsilon = pi/2-0.01;
+k_gains = [ 32 0.25 2.55 0.3]; % sort of tuned
+epsilon = pi/2-0.05;
 int_val = [0 0]; %integration value
 e_prev = [0 0];
 current_waypoint = 1;
 v_path = 0;
 phi_path = 0;
 for t = 2:length(T)
-    % v_path = 1;
-    % phi_path = pi/10*cos(T(t-1));
     [v_path, phi_path, int_val, e_prev, current_waypoint] = PID(X(1:7,t-1), t , int_val,k_gains,epsilon,e_prev,waypoints,current_waypoint, v_path,phi_path);
-    another_test(t) = phi_path;
     [X_Dot, V_1_2] = transf_func(X(1:7,t-1), [v_path,phi_path]);
     X(1:5,t) = X(1:5,t-1) + dt*X_Dot;
     X(6:7,t) = V_1_2;
@@ -389,11 +319,7 @@ function drawRobotsystem(x, y, theta, phi1, phi2, trailer1_x, trailer1_y, traile
     global Lw2;
     global Lh2;
     global dt;
-    % 
-    % trailer_length = 0.5;
-    % trailer_width = 0.3;
-    % tractor_length = 0.5;
-    % tractor_width = 0.3;
+
 
     % Get half dimensions for robot/tractor and trailers
     half_length = tractor_length / 2;
@@ -457,7 +383,7 @@ function drawRobotsystem(x, y, theta, phi1, phi2, trailer1_x, trailer1_y, traile
     
     axis equal;
 end
-for i = 150:length(T)
+for i = 1:length(T)
     clf;
     hold on;
     xlim([-15 20]); ylim([-10 12]);
@@ -479,21 +405,3 @@ for i = 150:length(T)
     pause(0.0001);
 
 end
-% close all
-% plot(testing_desired_angle)
-% hold on
-% plot(theta_0)
-% xlim([0 150])
-figure;
-plot(T,distance_error)
-hold on
-plot(T,angle_error)
-% title("Errors")
-% legend('Distance Error', 'Angle Error');
-% xlabel("Time (s)")
-% ylabel("Amplitude")
-
-
-plot(T,testing_desired_angle(2,:))
-hold on
-plot(T,testing_desired_angle(1,:))
